9c52532ac9fa72f576e9620121fc897d09a74afc
“username” <emailaddr>
2023-03-09 21:18:22 +0400

    srj

--- app/views.py
+++ app/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 def function(request):
     return render(request,'index.html')
 
+def about(request):
+    return render(request,'about.html')
+
 # Create your views here.
+
+
 
 
 
