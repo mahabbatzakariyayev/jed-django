875fec7354d6d06f1f41636df7118282ced016ba
“username” <emailaddr>
2023-03-09 22:04:44 +0400

    dinamik url ders sonu

--- app/views.py
+++ app/views.py
@@ -7,6 +7,12 @@ def about(request,soz):
     
     context = {
         "sozum":soz ,
-        "ad": str('salam')
+      
     }
-    return render(request,'about.html',context)
+    return render(request,'about.html',context =context)
+
+def jed(request):
+    c1 = {
+        'meyve': 'banan',
+    }
+    return render(request,'index.html',context=c1,)
