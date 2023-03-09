f300288933e8aa7e7c3498b85204cc663a6f16b2
“username” <emailaddr>
2023-03-09 21:02:01 +0400

    dinamik url

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
-def about(request):
-    return render(request,'about.html')
+def about(request,soz):
+    soz1 = soz
+    context = {
+        "sozum":soz1
+    }
+    return render(request,'about.html',context)
