abeddfc973ebae7223a7343a684678abc46ccb30
“username” <emailaddr>
2023-03-11 11:24:39 +0400

    first

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+   name= models.CharField("Ad", max_length=255)
+   create_date= models.DateTimeField("yaranma tarixi",auto_now_add=True)
+
