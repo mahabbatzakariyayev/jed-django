286c72960480fbd318e1153790b2f661d806c5fe
“username” <emailaddr>
2023-03-11 10:42:13 +0400

    9

--- app/views.py
+++ app/views.py
@@ -1,9 +1,6 @@
 
-<<<<<<< HEAD
-=======
-from django.shortcuts import render
 
->>>>>>> 7daf196bb735e9a7398ba8be92db94ed8883cbc3
+
 # Create your views here.
 
 from django.shortcuts import render,HttpResponse
@@ -15,13 +12,6 @@ def about(request,soz):
     
     context = {
         "sozum":soz ,
-<<<<<<< HEAD
-        "ad": str('salam')
-    }
-    return render(request,'about.html',context)
-
-
-=======
       
     }
     return render(request,'about.html',context =context)
@@ -31,5 +21,4 @@ def jed(request):
         'meyve': 'banan',
     }
     return render(request,'index.html',context=c1,)
->>>>>>> 7daf196bb735e9a7398ba8be92db94ed8883cbc3
 
